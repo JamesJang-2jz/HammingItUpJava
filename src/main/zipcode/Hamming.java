9769acd1174f6d29cb83aa5fc675935cc87c5246
James <james@Zipcoders-MacBook-Pro-101.local>
2023-04-10 18:33:42 -0400

    added constructor;

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,7 +1,14 @@
 package zipcode;
 
 public class Hamming {
-    public Hamming(String s, String s1) {
+    private String dna1;
+    private String dna2;
+    public Hamming(String dna1, String dna2) {
+        this.dna1 = dna1;
+        this.dna2 = dna2;
+        if (dna1.length() != dna2.length()){
+            throw new IllegalArgumentException();
+        }
     }
 
     public int getHammingDistance() {
